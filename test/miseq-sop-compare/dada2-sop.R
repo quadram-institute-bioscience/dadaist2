67fb439bbd887dc6575f22dd29e60f43955c97f6
Andrea Telatin <andrea@telatin.com>
2021-04-16 11:39:26 +0100

    sop

--- test/miseq-sop-compare/dada2-sop.R
+++ test/miseq-sop-compare/dada2-sop.R
@@ -16,12 +16,15 @@ cat(" Trunc1: ", tl1, "\n")
 cat(" Trunc2: ", tl2, "\n")
 
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
+cat("# Retrieving files\n")
 fnFs <- sort(list.files(path, pattern="_R1_001.fastq", full.names = TRUE))
 fnRs <- sort(list.files(path, pattern="_R2_001.fastq", full.names = TRUE))
 # Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
 sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
 
 library(dada2); packageVersion("dada2")
+
+cat("# Quality plots\n")
 pdf("for.pdf")
 plotQualityProfile(fnFs[1:2])
 dev.off()
@@ -31,27 +34,33 @@ plotQualityProfile(fnRs[1:2])
 dev.off()
 
 # Place filtered files in filtered/ subdirectory
+cat("# Filter and trim\n")
 filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
 filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
 out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(tl1,tl2),
               maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
               compress=TRUE, multithread=TRUE) # On Windows set multithread=FALSE
 
+cat("# Learn errors\n")
 errF <- learnErrors(filtFs, multithread=TRUE)
 errR <- learnErrors(filtRs, multithread=TRUE)
 
 # Derep
+cat("# Dereplicate\n")
 derepFs <- derepFastq(filtFs, verbose=TRUE)
 derepRs <- derepFastq(filtRs, verbose=TRUE)
 # Name the derep-class objects by the sample names
 names(derepFs) <- sample.names
 names(derepRs) <- sample.names
 
+cat("# DADA2\n")
 dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
 dadaRs <- dada(derepRs, err=errR, multithread=TRUE)
 
+cat("# Merge\n")
 mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose=TRUE)
 seqtab <- makeSequenceTable(mergers)
+cat("# Remove chimeras\n")
 seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE, verbose=TRUE)
 
 
